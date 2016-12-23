887c50e9dc1b041bfebc4c01a743a01e57b6462d
FidFenix <fidel1forever@gmail.com>
2016-12-23 14:56:07 -0500

    ...

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var ctrlPosts = require('../controllers/posts');
 var ctrlReviews = require('../controllers/reviews');
 
-router.get('/posts', ctrlLocations.postsList);
-router.post('/posts', ctrlLocations.postsCreate);
-router.get('/posts/:postid', ctrlLocations.postsReadOne);
-router.put('/posts/:postid', ctrlLocations.postsUpdateOne);
-router.delete('/posts/:postid', ctrlLocations.postsDeleteOne);
+router.get('/posts', ctrlPosts.postsList);
+router.post('/posts', ctrlPosts.postsCreate);
+router.get('/posts/:postid', ctrlPosts.postsReadOne);
+router.put('/posts/:postid', ctrlPosts.postsUpdateOne);
+router.delete('/posts/:postid', ctrlPosts.postsDeleteOne);
 
 // reviews
 router.post('/posts/:postid/reviews', ctrlReviews.reviewsCreate);
